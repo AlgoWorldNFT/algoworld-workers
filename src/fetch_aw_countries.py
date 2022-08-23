ae91a49c2f193272406d83305c0978e8a864917b
Altynbek Orumbayev <aorumbayev@protonmail.com>
2022-08-23 23:07:25 +0200

    adding country fetching workflow

--- src/fetch_aw_countries.py
+++ src/fetch_aw_countries.py
@@ -1,8 +1,35 @@
+import base64
+
 from algosdk.v2client.indexer import IndexerClient
 
 from models import AlgoWorldAsset
 from src.common import ALL_COUNTRIES_PATH, indexer
 from src.utils import save_aw_assets
+
+
+def fetch_country_image_url(
+    indexer: IndexerClient, creator_address: str, asset_index: int
+):
+    note_txns = indexer.search_transactions(
+        address=creator_address,
+        asset_id=asset_index,
+        limit=100,
+        min_amount=0,
+        txn_type="axfer",
+    )
+    if asset_index == 188042721:
+        print("stop")
+    if "transactions" in note_txns:
+        for txn in note_txns["transactions"]:
+            if "note" in txn:
+                try:
+                    note_content = str(base64.b64decode(txn["note"]))
+                    if "ipfs://" in note_content:
+                        cid = note_content.split("\\xaf3")[0].split("ipfs://")[1]
+                        return f"https://ipfs.io/ipfs/{cid}"
+                except Exception as exp:
+                    print(f"Unable to decode note for {asset_index} {exp} skipping")
+    return None
 
 
 def fetch_aw_countries(indexer: IndexerClient, creator_address: str):
@@ -28,6 +55,19 @@ def fetch_aw_countries(indexer: IndexerClient, creator_address: str):
         created_assets = indexer.search_assets(
             limit=100, creator=creator_address, next_page=created_assets["next-token"]
         )
+
+    all_assets_with_images = []
+
+    for asset in all_assets:
+        image_url = fetch_country_image_url(indexer, creator_address, asset.index)
+
+        if image_url:
+            asset.url = image_url
+        else:
+            print(f"No image found for {asset.name} adding as is anyway")
+
+        print(f"{asset.name} {asset.url} processed")
+        all_assets_with_images.append(asset)
 
     return all_assets
 
